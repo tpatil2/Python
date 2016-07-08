9273ed82134c96d14152514e0a911a0742535f68
Tejas Patil <tpatil2@mail.csuchico.edu>
2016-07-08 11:02:42 -0700

    basic

--- hellopython.py
+++ hellopython.py
@@ -2,7 +2,8 @@ import random
 import sys
 import os
 
-print ("Hello welcome to python\n")
+print ("\nHello welcome to python\n")
+
 
 name = "Tejas Patil"
 x=9829023358
@@ -10,22 +11,30 @@ print(name,"\n")
 print(x,"\n")
 
 
-print("5+2 =", 5+2)
+print("5+2 =", 5+2,"\n")
 print("5/2 =", 5/2)
 print("5*2 =", 5*2)
 print("5-2 =", 5-2)
 print("5**2 =", 5**2)
 print("5//2 =", 5//2) # prints only integer part
 
+'''
+multiline comment
+
+'''
+
 #order of operation
 # **
 # */+-
 print("6/3+5+1-2*2 =", 2**4/2+5+1-2*2)
 
-print("6/3+(5+1-2)*2 =", 6/3+(5+1-2)*2)
+print("6/3+(5+1-2)*2 =", 6/3+(5+1-2)*2,'\n')
 
 quote= "\"Hello there\""
 
-multiline_quote=''' This is multiline quote'''
+multiline_quote='''This is multiline quote'''
 
 print(quote,multiline_quote)
+
+print("This is first line",end='\n'*5)
+print("newline")
