4e9e8cd1300f0ccbec201c5a40f0f0b2482f5c01
Tejas Patil <tpatil2@mail.csuchico.edu>
2017-03-29 02:43:55 -0700

    Automate Parsing and Renaming of Multiple Files

---
+++ Automate Parsing and Renaming of Multiple Files/script_rename.py
@@ -0,0 +1,35 @@
+# write a pythin script to rename all files in a sequence
+
+import os
+
+# switch directory
+
+os.chdir('/Users/Tejas/my pythom/Python/Automate Parsing and Renaming of Multiple Files/files')
+
+print(os.getcwd()) #prints current working directory
+
+for f in os.listdir():
+    #split file name and extension
+    f_name, f_ext = os.path.splitext(f)
+
+    #split f_name in three seperte strings
+
+    f_auth, f_course, f_num = f_name.split("-")
+
+    '''
+    print(f_auth)
+    print(f_course)
+    print(f_num)
+    '''
+    #get rid of # at the begining
+    f_num = f_num.strip()[1:]
+    #convert into double digit
+    f_num=f_num.zfill(2)
+
+    new_name = '{}-{}-{}.{}'.format(f_num,f_auth,f_course,f_ext)
+    print(new_name)
+
+    os.rename(f,new_name)
+
+
+
