61fbddb7f3a49e73bfcf1a2fafa16f46ddfee3f6
Tejas Patil <tpatil2@mail.csuchico.edu>
2017-04-07 02:20:44 -0700

    Crawl Add Posting Time

--- Web Crawler/main_crawl.py
+++ Web Crawler/main_crawl.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-
+Title = ""
 def tread_spider(page_num):
     s=120
     while s <= page_num:
@@ -12,12 +12,24 @@ def tread_spider(page_num):
         for link in soup.findAll('a', {'class': 'result-title hdrlnk'}):
             href = "https://sfbay.craigslist.org" + link.get('href')
             Title = link.string
-            print(href)
-            print(Title)
+            #print(href)
+            #print(Title)
             file.write(href +" " + Title + '\n')
+            get_item_info(href,Title)
+            page_num = 122
 
-        page_num = 122
+
+
+def get_item_info(item_url,title):
+    response = requests.get(item_url)
+    plain_responce = response.text
+    soup = BeautifulSoup(plain_responce)
+    for item_desc in soup.findAll('time', {'class': 'timeago'}):
+        #print(item_desc.string)
+        file2.write(title + item_desc.string + '\n')
 
 
 file = open('data.txt', 'w')
+file2 = open('time.txt', 'w')
+
 tread_spider(120)
